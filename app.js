ccdf52bb88eaebab3e7c9a88d17eef8961edc77b
baejoonsoo <artns25@dsm.hs.kr>
2021-12-18 01:44:03 +0900

    draw

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 const onMouseMove = (event) => {
   const x = event.offsetX;
   const y = event.offsetY;
-  // console.log(`{${x},${y}}`);
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 };
 
 const onMouseDown = () => {
@@ -16,13 +30,13 @@ const stopPainting = () => {
   painting = false;
 };
 
-const onMouseUp = () => {
-  stopPainting;
+const startPainting = () => {
+  painting = true;
 };
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
